5211fe1ca6b11934221d4a1b6fa6aace93189520
dbornilla <78960805+dbornilla@users.noreply.github.com>
2023-05-22 16:06:41 +0800

    try

---
+++ src/register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./register.css"
+
+const RegisterForm = () => {
+    return(
+        <div classname="cover">
+            <h1> Login </h1>
+            <input type="text" placeholder="email" />
+            <input type="password" placeholder="password" />
+        </div>
+    )
+}
+
+export default RegisterForm
