647cec39f796d5060b55bdf1e0cbbc3792943a82
dbornilla <78960805+dbornilla@users.noreply.github.com>
2023-05-28 22:23:49 +0800

    book

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Form from './form.js';
 import Account from './account.js';
-import Location from './favoriteprof.js';
+import Favorite from './favoriteprof.js';
 
 function App() {
   return (
     <div className="App">
-      <Location />
+      <Favorite />
     </div>
   );
 }
