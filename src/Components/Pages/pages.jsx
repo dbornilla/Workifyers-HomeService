b5e88ccde39f5df75b83aae5bf89a539e10a12fd
dbornilla <78960805+dbornilla@users.noreply.github.com>
2023-06-15 07:45:12 +0800

    pages design

--- src/Components/Pages/pages.jsx
+++ src/Components/Pages/pages.jsx
@@ -1,4 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
+import './pages.css'
 
-import 'pages.css'
+
+import Home from "../Home/Home";
+import About from "../About/About";
+import Main from "../Main/Main";
+import Testimonies from "../Testimonies/Testimonies";
+import Experience from "../Experience/Experience";
+
+const Pages = () => {
+    return(
+        <div className="container-page mx-auto">
+            <Home />
+            <About />
+            <Main />
+            <Testimonies />
+            <Experience />
+        </div>
+    );
+}
+
+export default Pages
