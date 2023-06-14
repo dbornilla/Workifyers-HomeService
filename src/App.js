c1b616b370e194a3fc419ff9739f2c52f56e1086
dbornilla <78960805+dbornilla@users.noreply.github.com>
2023-06-14 15:56:34 +0800

    pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
 import './App.css';
 import './app1.css'
 
@@ -11,20 +11,25 @@ import Main from './Components/Main/Main';
 import Testimonies from './Components/Testimonies/Testimonies'
 import Footer from './Components/Footer/Footer';
 import Experience from './Components/Experience/Experience';
-
+import Account from './Components/ProfileManagement/account';
+import Booked from './Components/ProfileManagement/bookhistory';
+import Location from './Components/ProfileManagement/favoriteprof';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Main/>
-      <Testimonies/>
-      <Experience/>
-      <Footer/>
-      
-    </div>
+    
+      <div className="App">
+        <Navbar/>
+        <Home />
+        
+        <About/>
+        <Main/>
+        <Testimonies/>
+        <Experience/>
+        <Footer/>
+        
+      </div>
+    
   );
 }
 
